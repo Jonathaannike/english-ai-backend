08abe4ebd372de57b27ceec31571bcfacafe14ea
Jonathaannike <jonathaannike@hotmail.com>
2025-04-13 20:27:42 -0400

    new DB

--- database.py
+++ database.py
@@ -2,20 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Database URL, replace with your database URL (this is for SQLite example)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# URL de conexión a tu base de datos en Render
+DATABASE_URL = "postgresql://english_ai_db_user:DLJ3OkmshpcGg7Jzi4L6TsyrlUf38ejZ@dpg-cvu58qh5pdvs73e4tla0-a.oregon-postgres.render.com:5432/english_ai_db"
 
-# Create engine and session
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Create Base class to use in your models
 Base = declarative_base()
-
-# Dependency to get the DB session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
