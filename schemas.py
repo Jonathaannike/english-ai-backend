c7b49ef40bc83ac5eb7cce70cc89085422026d7d
Jonathaannike <jonathaannike@hotmail.com>
2025-04-14 08:16:01 -0400

    schemasupdate

--- schemas.py
+++ schemas.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel
 
+# Esquema para los datos de creación de un usuario
+class UserCreate(BaseModel):
+    email: str
+    password: str  # La contraseña del usuario
+
+# Esquema para el token de acceso (ya tienes este)
 class Token(BaseModel):
     access_token: str
     token_type: str
